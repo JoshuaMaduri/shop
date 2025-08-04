302b11d74f5df1d26a0c07238aae9d50209686d2
Joshua maduri <jmaduri@hotmail.com>
2025-08-03 22:16:17 -0400

    Removed Next.JS Files and added signout button from clerk

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,21 @@
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { currentUser } from "@clerk/nextjs/server";
+import { SignOutButton } from "@clerk/nextjs";
 
 export default async function Page() {
-    const { user } = await auth();
+    const user = await currentUser();
     return (
         <div>
             <h1>Welcome to the Dashboard</h1>
             {user ? (
-                <p>Hello, {user.firstName} {user.lastName}!</p>
+                <div>
+                    <p>Hello, {user.firstName} {user.lastName}!</p>
+                    <SignOutButton>
+                        <button>Sign Out</button>
+                    </SignOutButton>
+                </div>
+                
+                
+
             ) : (
                 <p>Please sign in to access your dashboard.</p>
             )}
